7ae23e028e597272d062fcb26d5cef8d216b3d41
Johnle <memetintin12345@gmail.com>
2021-08-14 17:41:13 +0700

    final add

--- web/app.py
+++ web/app.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat May 23 19:08:16 2020
-
-@author: HP PC
-"""
-
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
@@ -23,24 +16,13 @@ def predict():
     For rendering results on HTML GUI
     '''
     int_features = [int(x) for x in request.form.values()]
+    print(int_features)
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
     output = round(prediction[0], 2)
 
-    return render_template('giaodien.html', prediction_text='Giá bất động sản dự đoán $ {}'.format(output))
-
-
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
-
-    output = prediction[0]
-    return jsonify(output)
+    return render_template('giaodien.html', prediction_text='Giá bất động sản dự đoán tính theo USD {}'.format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
